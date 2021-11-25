f4cd2bf7dcb80bedcab1108868c3128698f61ff6
Yngve Gustafsson <yngve.gustafsson@sogeti.com>
2021-11-25 07:32:50 +0100

    OK

--- Pull-Request-Test/Pull-Request-Test/Controllers/WeatherForecastController.cs
+++ Pull-Request-Test/Pull-Request-Test/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Pull_Request_Test.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "XXX", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hua vad kallt", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
